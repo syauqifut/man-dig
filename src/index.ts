8e8697ca031190f221b2ab9d1378d763db18983a
syauqifut <syauqifut@gmail.com>
2024-08-03 12:51:22 +0700

    chore: init and move to express

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,45 @@
-import puppeteer from "puppeteer";
+import express from "express";
 import { scrapeFilmLists, scrapeAnimeLists, scrapeMangaLists, scrapeBookLists, scrapeGameLists } from "./scrapper";
 import { SearchResult } from "./models/searchResult";
 
-async function main() {
-  const searchQuery = process.argv[2].trim();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/search", async (req, res) => {
+  const searchQuery = req.query.q as string;
 
   if (!searchQuery) {
-    console.error("Please provide a search query as an argument.");
-    process.exit(1);
+    return res.status(400).json({ error: "Please provide a search query as a 'q' parameter." });
   }
 
   console.log(`Searching for: "${searchQuery}"`);
 
-  const browser = await puppeteer.launch();
+  try{
+    const fetchFunctions: { [key: string]: (searchQuery: string) => Promise<SearchResult[]> } = {
+      films: scrapeFilmLists,
+      animes: scrapeAnimeLists,
+      mangas: scrapeMangaLists,
+      books: scrapeBookLists,
+      games: scrapeGameLists,
+    };
 
-  const fetchFunctions: { [key: string]: (searchQuery: string) => Promise<SearchResult[]> } = {
-    films: scrapeFilmLists,
-    animes: scrapeAnimeLists,
-    mangas: scrapeMangaLists,
-    books: scrapeBookLists,
-    games: scrapeGameLists,
-  };
+    const results = await Promise.all(
+      Object.keys(fetchFunctions).map(async (key) => {
+        const fetchFunction = fetchFunctions[key];
+        const data = await fetchFunction(searchQuery);
+        return { key, data: data.slice(0, 3) };
+      })
+    );
 
-  const results = await Promise.all(
-    Object.keys(fetchFunctions).map(async (key) => {
-      const fetchFunction = fetchFunctions[key];
-      const data = await fetchFunction(searchQuery);
-      return { key, data: data.slice(0, 3) };
-    })
-  );
+    res.json(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while processing your request." });
+  }
+});
 
-  results.forEach(({ key, data }) => {
-    console.log(`${key.charAt(0).toUpperCase() + key.slice(1)}:`);
-    console.log(data);
-  });
-
-  await browser.close();
-  process.exit(1);
-}
-
-
-main().catch(console.error);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
